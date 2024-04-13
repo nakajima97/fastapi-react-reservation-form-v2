b8694b1da9ddd9b65c6fc8cc01064aee2a8c97f2
Yudai Nakajima <kituneudon97@gmail.com>
2024-04-13 17:39:32 +0900

    #5 テーブル設計とマイグレーションの作成

--- api/source/migrate_db.py
+++ api/source/migrate_db.py
@@ -1,13 +1,16 @@
 from sqlalchemy import create_engine
 
 from source.models.reservations import Reservations
+from source.models.calendars import Calendars
 
 DB_URL = "mysql+pymysql://root@mysql:3306/reservation_form"
 engine = create_engine(DB_URL, echo=True)
 
 def reset_database():
   Reservations.metadata.drop_all(engine)
+  Calendars.metadata.drop_all(engine)
   Reservations.metadata.create_all(engine)
+  Calendars.metadata.create_all(engine)
 
 if __name__ == "__main__":
   reset_database()
