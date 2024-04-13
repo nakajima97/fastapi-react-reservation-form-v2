1cd2347976e38a32ec45883a73dc9eeaae67edf1
Nakajima <kituneudon97@gmail.com>
2024-04-13 15:09:30 +0900

    POST /reservations のテストコード作成 (#22)
    * #7 POST /reservationsを実行した際に200が返ってくることを検証するテストコードを作成
    
    * #7 レスポンスの中身があっていることを検証

---
+++ api/tests/test_main.py
@@ -0,0 +1,47 @@
+import pytest
+import pytest_asyncio
+import starlette.status
+
+from httpx import AsyncClient
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+
+from source.db import get_db, Base
+from source.main import app
+
+ASYNC_DB_URL = "sqlite+aiosqlite:///:memory:"
+
+@pytest_asyncio.fixture
+async def async_client() -> AsyncClient:
+    async_engine = create_async_engine(ASYNC_DB_URL, echo=True)
+    async_session = sessionmaker(autocommit=False, autoflush=False, bind=async_engine, class_=AsyncSession)
+
+    async with async_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+    async def get_test_db():
+        async with async_session() as session:
+            yield session
+
+    app.dependency_overrides[get_db] = get_test_db
+
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        yield client
+
+@pytest.mark.asyncio
+async def test_create_reservation(async_client):
+    base_json = {
+        "date": "2024-01-01",
+        "name": "テスト　ヨヤク",
+        "email_address": "example@example.com",
+        "phone_number": "123-456-7890"
+    }
+    response = await async_client.post("/reservations", json=base_json)
+    assert response.status_code == starlette.status.HTTP_200_OK
+    response_object = response.json()
+    assert response_object["date"] == base_json["date"]
+    assert response_object["name"] == base_json["name"]
+    assert response_object["email_address"] == base_json["email_address"]
+    assert response_object["phone_number"] == base_json["phone_number"]
+    assert response_object["id"] == 1
