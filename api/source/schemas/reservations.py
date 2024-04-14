7a29631797b8a65d064f3d729cd8a682f343ad54
nakajima97 <kituneudon97@gmail.com>
2024-04-15 08:43:41 +0900

    #31 500エラーを解消

--- api/source/schemas/reservations.py
+++ api/source/schemas/reservations.py
@@ -2,10 +2,37 @@ from pydantic import BaseModel, Field
 import datetime
 
 class Reservation(BaseModel):
-    date: datetime.date = Field(..., title="Date of the reservation", examples="2024-01-01")
-    name: str = Field(..., title="Name of the person making the reservation", examples="テスト　ヨヤク")
-    email_address: str = Field(..., title="Email of the person making the reservation", examples="example@example.com")
-    phone_number: str = Field(..., title="Phone number of the person making the reservation", examples="123-456-7890")
+    date: datetime.date = Field(..., title="Date of the reservation")
+    name: str = Field(..., title="Name of the person making the reservation")
+    email_address: str = Field(..., title="Email of the person making the reservation")
+    phone_number: str = Field(..., title="Phone number of the person making the reservation")
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "date": "2024-04-13",
+                    "name": "John Doe",
+                    "email_address": "example@example.com",
+                    "phone_number": "123-4567-8901",
+                }
+            ]
+        }
+    }
 
 class ResponseReservation(Reservation):
-    id: int = Field(..., title="ID of the reservation", examples=1)
+    id: int = Field(..., title="ID of the reservation")
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "id": 1,
+                    "date": "2024-04-13",
+                    "name": "John Doe",
+                    "email_address": "example@example.com",
+                    "phone_number": "123-4567-8901",
+                }
+            ]
+        }
+    }
