2801997a4de48a5c12b1b3e9f3939ad0ea5ad326
Nakajima <kituneudon97@gmail.com>
2024-04-18 08:06:10 +0900

    calendars.dateにユニーク制約を追加 (#36)

--- api/source/models/calendars.py
+++ api/source/models/calendars.py
@@ -10,7 +10,7 @@ class Calendars(Base):
     __tablename__ = 'calendars'
 
     id = Column(Integer, primary_key=True, index=True)
-    date = Column(Date, nullable=False)
+    date = Column(Date, nullable=False, unique=True)
     is_holiday = Column(Boolean, nullable=False, default=False)
     created_at = Column(DateTime, nullable=False, default=func.now())
     updated_at = Column(DateTime, nullable=False, default=func.now())
