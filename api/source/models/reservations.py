cafdd5a1953640a4a393309d3aacbe62bd7f3a70
Nakajima <kituneudon97@gmail.com>
2024-04-18 08:54:40 +0900

    reservatinsテーブルにcreated_atとupdated_atを追加した (#37)

--- api/source/models/reservations.py
+++ api/source/models/reservations.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String, Date
+from sqlalchemy import Column, Integer, String, Date, DateTime
+from sqlalchemy.sql import func
 
 from source.db import Base
 
@@ -10,3 +11,5 @@ class Reservations(Base):
   name = Column(String(255))
   email_address = Column(String(255))
   phone_number = Column(String(15))
+  created_at = Column(DateTime, nullable=False, default=func.now())
+  updated_at = Column(DateTime, nullable=False, default=func.now())
