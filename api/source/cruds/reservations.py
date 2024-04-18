3069de6f4647042aefc5b4859f3c5c9d555c2c9c
Nakajima <kituneudon97@gmail.com>
2024-04-18 21:25:23 +0900

    GET /holidays でDBから値を取得するようにした (#40)

--- api/source/cruds/reservations.py
+++ api/source/cruds/reservations.py
@@ -1,4 +1,5 @@
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.sql import select
 
 import source.models.reservations as reservations_model
 import source.schemas.reservations as reservations_schema
@@ -9,3 +10,10 @@ async def store_reservations(db: AsyncSession, reservation: reservations_schema.
     await db.commit()
     await db.refresh(reservation)
     return reservation
+
+async def fetch_reservations(db):
+    result = await db.execute(select(reservations_model.Reservations))
+    result_all = result.all()
+    reservations = [reservation[0] for reservation in result_all]
+
+    return {'reservations': reservations}
