b4f8492621c6285b59d6a0b1d52a2c660622e133
Yudai Nakajima <kituneudon97@gmail.com>
2024-04-20 11:37:20 +0900

    #49 ヘッダーのトークンによる認証を実装

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 `docker compose build`  
 `docker compose run --entrypoint "poetry install --no-root" api`  
 `docker compose run --entrypoint "yarn install" front`  
+`docker compose up`  
+`docker compose exec api cp source/config.py.sample source/config.py`  
 
 マイグレーションの実行  
 `docker compose exec api poetry run python -m source.migrate_db`  
--- api/source/routers/holidays.py
+++ api/source/routers/holidays.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 from source.db import get_db
 from source.schemas.holidays import Holidays
 from source.cruds.calendars import store_holidays, fetch_holidays
+from source.authentication import get_api_key_from_header
 
 router = APIRouter()
 
@@ -12,6 +13,6 @@ async def get_holidays(db: Session = Depends(get_db)):
   return await fetch_holidays(db)
 
 @router.post("/holidays", response_model=Holidays)
-async def post_holidays(holidays: Holidays, db: Session = Depends(get_db)):
+async def post_holidays(holidays: Holidays, db: Session = Depends(get_db), api_key: str = Depends(get_api_key_from_header)):
   calendar = await store_holidays(db, holidays)
   return calendar
